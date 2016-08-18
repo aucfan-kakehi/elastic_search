6ee22cabf6562b379680ebe2a782dff46b2b459f
aucfan-kakehi <kakehi@aucfan.com>
2016-08-18 13:27:19 +0900

    インストールファイル追加

---
+++ install_es.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+
+## es install
+sudo rpm -Uvh https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.0/elasticsearch-2.3.0.rpm
---
+++ install_java.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+## java install
+wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm
+sudo rpm -Uhv jdk-8u45-linux-x64.rpm
+
